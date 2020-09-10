cd7733547c4d728ac0fa061bcff12431b6956cf7
quinnfinite <quinn.royston@gmail.com>
2020-09-09 17:03:57 -0700

    Stopped at Step 3. Write Query Resolvers

---
+++ start/server/src/resolvers.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Query: {
+    launches: (_,__,{dataSources}) =>
+      dataSources.launchAPI.getAllLaunches(),
+    launch: (_,{id}, {dataSources}) => dataSources.launchAPI.getLaunchById({launchId: id}),
+    me: (_,__,{ dataSources }) => dataSources.launchAPI.findOrCreateUser()
+  }
+}
